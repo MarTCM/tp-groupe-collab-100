e30905a3d43e728279f72c2950524311ca7e2bb1
MarTCM <marwane.elbaraka@gmail.com>
2026-03-18 17:36:32 +0000

    feat(calculator): Création de la fonction puissance (#2)

--- src/calculator.py
+++ src/calculator.py
@@ -6,9 +6,5 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def divide(a, b):
-    """Retourne la division de a par b. Lance une exception"""
-    """si elle detecte une erreur."""
-    if b == 0:
-        raise ValueError("Division par zero est impossible")
-    return a / b
+def power(base, exp):
+    return base ** exp
